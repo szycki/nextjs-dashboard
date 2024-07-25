2991b3c60f47d53642cab97381b9cf1879864f34
szycki <simon.zycki@gmail.com>
2024-07-25 14:37:48 +0200

    change formatting

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,13 +1,13 @@
-'use client';
+'use client'
 
 import {
 	UserGroupIcon,
 	HomeIcon,
 	DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
+} from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -19,15 +19,15 @@ const links = [
 		icon: DocumentDuplicateIcon,
 	},
 	{ name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-];
+]
 
 export default function NavLinks() {
-	const pathname = usePathname();
+	const pathname = usePathname()
 
 	return (
 		<>
-			{links.map((link) => {
-				const LinkIcon = link.icon;
+			{links.map(link => {
+				const LinkIcon = link.icon
 				return (
 					<Link
 						key={link.name}
@@ -39,12 +39,12 @@ export default function NavLinks() {
 									pathname === link.href,
 							}
 						)}>
-						<LinkIcon className="w-6" />
-						<p className="hidden md:block">{link.name}</p>
+						<LinkIcon className='w-6' />
+						<p className='hidden md:block'>{link.name}</p>
 					</Link>
-				);
+				)
 			})}
 		</>
-	);
+	)
 }
 
